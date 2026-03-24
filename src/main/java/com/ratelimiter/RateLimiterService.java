7cc919adb010a74e173cd00c7c115be028d68be2
sevbad <sbadriddinova@usf.edu>
2026-03-24 13:47:50 -0400

    add prometheus metrics for allowed/rejected requests

--- src/main/java/com/ratelimiter/RateLimiterService.java
+++ src/main/java/com/ratelimiter/RateLimiterService.java
@@ -1,5 +1,8 @@
 package com.ratelimiter;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -10,7 +13,17 @@ import java.util.concurrent.ConcurrentHashMap;
 public class RateLimiterService {
     @Autowired
     RedisTemplate<String, Long> redisTemplate;
+    @Autowired
+    MeterRegistry meterRegistry;
     ConcurrentHashMap<String, TokenBucket> tokenBuckets;
+    Counter tokensAllowed;
+    Counter tokensRejected;
+
+    @PostConstruct
+    public void init(){
+        tokensAllowed = meterRegistry.counter("rate_limiter_allowed");
+        tokensRejected = meterRegistry.counter("rate_limiter_rejected");
+    }
 
     RateLimiterService() {
         this.tokenBuckets = new ConcurrentHashMap<>();
@@ -24,6 +37,13 @@ public class RateLimiterService {
             bucket = new TokenBucket(5, 2, userId, redisTemplate);
             tokenBuckets.put(userId, bucket);
         }
-        return bucket.allowRequestAtomic();
+        boolean result = bucket.allowRequestAtomic();
+        if (result){
+            tokensAllowed.increment();
+        }
+        else{
+            tokensRejected.increment();
+        }
+        return result;
     }
 }
